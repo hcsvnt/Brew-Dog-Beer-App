7ff57e89882c86f972e83694c0ad1c29eea48d1c
Hallala <mateuszhallala@gmail.com>
2021-04-16 00:27:24 +0200

    style now

--- components/filters/index.js
+++ components/filters/index.js
@@ -1,3 +1,10 @@
+// import SVG from '../svg';
+import { WeakABVSVG,
+        MediumABVSVG,
+        StrongABVSVG,
+        WeakIBUSVG,
+        MediumIBUSVG,
+        StrongIBUSVG, } from '../svg'
 import * as styles from './filters.module.css';
 
 const Filters = (props) => {
@@ -25,120 +32,116 @@ const Filters = (props) => {
 
     return (
         <div>
+            <div className={styles.container}>
+                <div className={styles.filter}>
+                    <p>ABV</p>
+                    <form id="filterABV" className={styles.filter__form} onChange={props.setABV}>
+                        <label>
+                        <input
+                            type="radio"
+                            name="filter-abv"
+                            className={styles.filter__all}
+                            id="abvAll"
+                            value="all"
+                            // checked
+                        />
+                        <span className={styles.filter__all}>All</span>
+                        </label>
+                        <label>
+                        <input
+                            type="radio"
+                            name="filter-abv"
+                            // class="beer"
+                            id="abvWeak"
+                            value="weak"
+                        />
+                        <WeakABVSVG />
+                        </label>
+                        <label>
+                        <input
+                            type="radio"
+                            name="filter-abv"
+                            // class="beer"
+                            id="abvMedium"
+                            value="medium"
+                        />
+                        <MediumABVSVG />
+                        </label>
+                        <label>
+                        <input
+                            type="radio"
+                            name="filter-abv"
+                            // class="beer"
+                            id="abvStrong"
+                            value="strong"
+                        />
+                        <StrongABVSVG />
+                        </label>
+                    </form>
+                </div>
+                <div className={styles.filter}>
+                    <p>IBU</p>
+                    <form id="filterIBU" className={styles.filter__form} onChange={props.setIBU}>
+                        <label>
+                        <input
+                            type="radio"
+                            name="filter-ibu"
+                            className={styles.filter__all}
+                            id="ibuAll"
+                            value="all"
+                            // checked
+                        />
+                        <span className={styles.filter__all}>All</span>
+                        </label>
+                        <label>
+                        <input
+                            type="radio"
+                            name="filter-ibu"
+                            // class="beer"
+                            id="ibuWeak"
+                            value="weak"
+                        />
+                        <WeakIBUSVG />
+                        </label>
+                        <label>
+                        <input
+                            type="radio"
+                            name="filter-ibu"
+                            // class="beer"
+                            id="ibuMedium"
+                            value="medium"
+                        />
+                        <MediumIBUSVG />
+                        </label>
+                        <label>
+                        <input
+                            type="radio"
+                            name="filter-ibu"
+                            // class="beer"
+                            id="ibuStrong"
+                            value="strong"
+                        />
+                        <StrongIBUSVG />
+                        </label>
+                    </form>
+                </div>
+                <div className={styles.filter__text}>
+                <p>Page: <span id="pageNumber">{page}</span></p>
+                </div>
+                <div className={styles.filter}>
+                <button id="prevPage" className={styles.page__btn} onClick={props.prevPage} 
+                disabled={prevDisabled}
+                >
+                    {/* <i class="far fa-caret-square-left"></i> */}
+                    <span>prev</span>
+                </button>
+                <button id="nextPage" className={styles.page__btn} onClick={props.nextPage} disabled={nextDisabled}>
+                    {/* <i class="far fa-caret-square-right"></i> */}
+                    <span>next</span>
+                </button>
+                </div>
 
-        <div className={styles.container}>
-            <p>filters go here</p>
-            
-            <div className={styles.filter}>
-                <p>ABV</p>
-                <form id="filterABV" className={styles.filter__form} onChange={props.setABV}>
-                    <label>
-                    <input
-                        type="radio"
-                        name="filter-abv"
-                        className={styles.filter__all}
-                        id="abvAll"
-                        value="all"
-                        // checked
-                    />
-                    <span className={styles.filter__all}>All</span>
-                    </label>
-                    <label>
-                    <input
-                        type="radio"
-                        name="filter-abv"
-                        // class="beer"
-                        id="abvWeak"
-                        value="weak"
-                    />
-                    <span>weak</span>
-                    </label>
-                    <label>
-                    <input
-                        type="radio"
-                        name="filter-abv"
-                        // class="beer"
-                        id="abvMedium"
-                        value="medium"
-                    />
-                    <span>medium</span>
-                    </label>
-                    <label>
-                    <input
-                        type="radio"
-                        name="filter-abv"
-                        // class="beer"
-                        id="abvStrong"
-                        value="strong"
-                    />
-                    <span>strong</span>
-                    </label>
-                </form>
             </div>
-            <div className={styles.filter}>
-                <p>IBU</p>
-                <form id="filterIBU" className={styles.filter__form} onChange={props.setIBU}>
-                    <label>
-                    <input
-                        type="radio"
-                        name="filter-ibu"
-                        className={styles.filter__all}
-                        id="ibuAll"
-                        value="all"
-                        // checked
-                    />
-                    <span className={styles.filter__all}>All</span>
-                    </label>
-                    <label>
-                    <input
-                        type="radio"
-                        name="filter-ibu"
-                        // class="beer"
-                        id="ibuWeak"
-                        value="weak"
-                    />
-                    <span>weak</span>
-                    </label>
-                    <label>
-                    <input
-                        type="radio"
-                        name="filter-ibu"
-                        // class="beer"
-                        id="ibuMedium"
-                        value="medium"
-                    />
-                    <span>medium</span>
-                    </label>
-                    <label>
-                    <input
-                        type="radio"
-                        name="filter-ibu"
-                        // class="beer"
-                        id="ibuStrong"
-                        value="strong"
-                    />
-                    <span>strong</span>
-                    </label>
-                </form>
-            </div>
-            <div className={styles.filter__text}>
-              <p>Page: <span id="pageNumber">{page}</span></p>
-            </div>
-            <div className={styles.filter}>
-              <button id="prevPage" className={styles.page__btn} onClick={props.prevPage} 
-              disabled={prevDisabled}
-              >
-                {/* <i class="far fa-caret-square-left"></i> */}
-                <span>prev</span>
-              </button>
-              <button id="nextPage" className={styles.page__btn} onClick={props.nextPage} disabled={nextDisabled}>
-                {/* <i class="far fa-caret-square-right"></i> */}
-                <span>next</span>
-              </button>
-            </div>
-
-        </div>
-
         </div>
     )
 }
